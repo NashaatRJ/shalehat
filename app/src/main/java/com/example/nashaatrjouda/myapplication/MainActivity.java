700458a9be12a6f581e2ffed2d016dde98ec25fc
NashaatRJ <nashaat.r.jouda@hotmail.com>
2018-08-14 15:57:25 +0300

    edit

--- app/src/main/java/com/example/nashaatrjouda/myapplication/MainActivity.java
+++ app/src/main/java/com/example/nashaatrjouda/myapplication/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.nashaatrjouda.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+    Timer timer;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask()
+        {
+            @Override
+
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+                finish();
+           }
+
+        },1500);
+
     }
+
+    public void nextpage(View view) {
+        Intent intent = new Intent(MainActivity.this,SignIn.class);
+        startActivity(intent);
+    }
+
+
+    //public void nextsignin(View view) {
+      //  Intent intent = new Intent(MainActivity.this, SignIn.class);
+      //  startActivity(intent);
+   // }
 }
